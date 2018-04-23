2f3da1d271dcf81e5189f5accdfd415da48377af
echobravo <eburghardt@pivotal.io>
2018-04-23 15:10:38 -0700

    GEODE-4889: lvalue instead of rvalue

--- cppcache/include/geode/DataInput.hpp
+++ cppcache/include/geode/DataInput.hpp
@@ -187,7 +187,7 @@ class APACHE_GEODE_EXPORT DataInput {
    */
   inline int32_t readInt32() {
     _GEODE_CHECK_BUFFER_SIZE(4);
-    auto tmp = *(m_buf++);
+    int32_t tmp = *(m_buf++);
     tmp = (tmp << 8) | *(m_buf++);
     tmp = (tmp << 8) | *(m_buf++);
     tmp = (tmp << 8) | *(m_buf++);
