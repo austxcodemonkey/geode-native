b0b5dc2a38953eef13184356526bc11364553ca8
Charlie Black <charliemblack@gmail.com>
2019-09-16 13:55:53 -0700

    GEODE-6781 - final possible on getting windows done with using org.apache.geode.security.SecurityManager

--- examples/cpp/authinitialize/stopserver.ps1
+++ examples/cpp/authinitialize/stopserver.ps1
@@ -35,8 +35,9 @@ else
 
 if ($GFSH_PATH -ne "")
 {
-   Invoke-Expression "$GFSH_PATH -e 'connect' -e 'destroy region --name=region' -e 'stop server --name=server' -e 'stop locator --name=locator'"
+   Invoke-Expression "$GFSH_PATH -e 'connect --locator=localhost[10334] --user=server --password=server' -e 'destroy region --name=region' -e 'shutdown --include-locators=true'"
 }
 
 Remove-Item -Path locator -Recurse -Force
 Remove-Item -Path server -Recurse -Force
+
