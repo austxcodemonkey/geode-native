a4aaa2595abe9b2b9f168c445f5343de22c9dd25
Blake Bender <bbender@pivotal.io>
2020-07-08 10:04:08 -0700

    GEODE-8336: Revert change to CLI integration test - ExpirationTestsN expects stats to be enabled

--- clicache/integration-test/ExpirationTestsN.cs
+++ clicache/integration-test/ExpirationTestsN.cs
@@ -51,7 +51,7 @@ namespace Apache.Geode.Client.UnitTests
     {
       base.InitTests();
       Properties<string, string> config = new Properties<string, string>();
-      config.Insert("statistic-sampling-enabled", "false");
+      config.Insert("statistic-sampling-enabled", "true");
       CacheHelper.InitConfig(config);
     }
 
